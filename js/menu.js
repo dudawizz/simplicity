bdcee20d583bd7ab9787e735a15639ee3525adc5
dudawizz <langereduarda7@gmail.com>
2024-04-30 16:25:21 -0300

    adicionando fechar no menu

---
+++ js/menu.js
@@ -0,0 +1,23 @@
+'use script'
+
+/* Selecionando o elemento que acionará a lista de links do menu */
+const botaoMenu = document.querySelector("nav h2 a");
+
+/* Selecionando a lista de links/menu */
+const listaDeLinks = document.querySelector(".links-menu");
+
+/* Monitorando o evento de click do botaoMenu */
+botaoMenu.addEventListener("click", function(event){
+    /* Prevenir/anular o evento padrão do link de redirecionamento/recarregamento da página*/
+    event.preventDefault();
+    
+    listaDeLinks.classList.toggle("aberto"); 
+
+    if (listaDeLinks.classList.contains("aberto")) {
+       botaoMenu.innerHTML = "Fechar &times;";
+    } else {
+        botaoMenu.innerHTML = "Menu &equiv;";
+    } //innerHTML é utilizado para reconhecer os símbolos
+
+});
+
