55384878db0587e26fab07178101b687e476b825
dudawizz <langereduarda7@gmail.com>
2024-05-09 17:15:53 -0300

    mudando a mensagem de erro e envio

--- js/contato.js
+++ js/contato.js
@@ -79,3 +79,36 @@ botaoBuscar.addEventListener("click", async function(event){
     }
 
 });
+
+/* Script do Formspree */    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status-do-envio");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: formulario.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Obrigado pela sua inscrição!";
+          status.style.color = "blue";
+          formulario.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! Houve um problema na sua inscrição :("
+              status.style.color = "red";
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! Houve um problema na sua inscrição :("
+        status.style.color = "red";
+      });
+    }
+    formulario.addEventListener("submit", handleSubmit)
